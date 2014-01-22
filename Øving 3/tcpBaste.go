906243c38fb3e1fcdfd72002867afb4b2b066a1b
basbru <basteb@stud.ntnu.no>
2014-01-22 11:53:43 +0100

    Create tcpBaste.go

---
+++ Øving 3/tcpBaste.go
@@ -0,0 +1,31 @@
+// Go 1.2
+// go run helloworld_go.go
+
+package main
+
+import (
+    . "fmt" // Using '.' to avoid prefixing functions with their package names
+    . "runtime" // This is probably not a good idea for large projects...
+    . "time"
+	"net"
+)
+
+var adr = 129.241.187.161
+var port = 33546
+
+
+addr = new InternetAddress(serverIP, serverPort) 
+sock = new Socket(tcp) // TCP, aka SOCK_STREAM
+sock.connect(addr)
+// use sock.recv() and sock.send()
+
+
+func main() {
+
+ResolveTCPAddr("tcp", adr) (*TCPAddr, error)
+DialTCP("tcp", NULL, raddr *TCPAddr) (*TCPConn, error)
+
+
+
+
+}
